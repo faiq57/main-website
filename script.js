914637de7812b2daf94696ba616e2bffd359fe44
Faiq Izhal <f.izhal@student.unsw.edu.au>
2026-03-03 06:01:33 +1100

    Made the site retrieve new blog post from d1 database instead of already having the posts in the html files

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
-const articles = document.getElementsByTagName('article')
-
-const addNewComment = async (author, content, articleID) => {
+const addNewComment = async (author, content, postId) => {
     const request = new Request("https://blog-data.phaeiq.com/comments", {
         method: "POST",
         body: JSON.stringify({
             'author': author,
-            'content': content
+            'content': content,
+            'postId': postId
         })
     })
     const response = await fetch(request)
 }
 
-const createCommentCollection = async () => {
-    const request = new Request("https://blog-data.phaeiq.com/comments", {
+const createCommentCollection = async (postId) => {
+    const request = new Request("https://blog-data.phaeiq.com/comments/" + postId.toString(), {
     method: "GET",
     });
 
@@ -54,7 +53,59 @@ const createCommentCollection = async () => {
     return commentCollection
 }
 
-for (let article of articles) {
+const mainContentContainer = document.getElementById('main-content-container')
+
+const request = new Request("https://blog-data.phaeiq.com/posts", {
+    method: "GET",
+})
+
+const response = await fetch(request)
+if (!response.ok) {
+    console.log("posts aren't loading, something is wrong")
+}
+const posts = await response.json()
+
+const artIds = []
+
+for (let post of posts) {
+    console.log(post)
+    let article = document.createElement('article')
+    article.id = 'article-' + post.id.toString()
+
+    // Put title in
+    let postTitle = document.createElement('h2')
+    postTitle.innerText = post.title
+    article.appendChild(postTitle)
+
+    let authorTimeLine = document.createElement('div')
+    authorTimeLine.classList.add('author-time-line')
+
+    // Put author name in
+    let authorLine = document.createElement('div')
+    authorLine.innerText = 'By ' + post.author
+    authorTimeLine.appendChild(authorLine)
+
+    // Put timestamp in
+    let postTime = document.createElement('div')
+    postTime.classList.add('timestamp')
+    postTime.innerText = post.timestamp
+    authorTimeLine.appendChild(postTime)
+
+    article.appendChild(authorTimeLine)
+
+    // Put content in
+    let postContent = document.createElement('div')
+    postContent.classList.add('content-container')
+    postContent.innerHTML = post.content
+    article.appendChild(postContent)
+
+    mainContentContainer.appendChild(article)
+    console.log(post.id)
+    artIds.push([article, post.id])
+}
+
+for (let artId of artIds) {
+    console.log(artId)
     // Made a comment section
     let newCommentSection = document.createElement('div')
     newCommentSection.classList.add('comments-section')
@@ -64,7 +115,7 @@ for (let article of articles) {
     commentHeader.innerText = 'Comments'
     newCommentSection.appendChild(commentHeader)
 
-    // *NOT FULLY IMPLEMENTED* textbox for posting comments
+    // textbox for posting comments
     let commentBox = document.createElement('div')
     commentBox.classList.add('comment-box')
     let nameLable = document.createElement('label')
@@ -79,18 +130,17 @@ for (let article of articles) {
     let commentSubmitButton = document.createElement('button')
     commentSubmitButton.innerText = 'Submit comment'
     commentSubmitButton.onclick = async () => {
-        await addNewComment(commentAuthorWriter.value, commentWriter.value, 0)
-        // *MUST BE EDITED LATER*
-        const replacementCommentSection = await createCommentCollection()
-        document.getElementsByClassName('comment-collection')[0].remove()
-        const commentSection = document.getElementsByClassName('comments-section')[0]
+        await addNewComment(commentAuthorWriter.value, commentWriter.value, artId[1])
+        const replacementCommentSection = await createCommentCollection(artId[1])
+        const commentSection = artId[0].lastElementChild
+        commentSection.lastElementChild.remove()
         commentSection.appendChild(replacementCommentSection)
     }
     commentBox.appendChild(commentSubmitButton)
     newCommentSection.appendChild(commentBox)
 
-    // Append all the relevant (not yet) comments
-    newCommentSection.appendChild(await createCommentCollection())
+    // Append all the relevant comments
+    newCommentSection.appendChild(await createCommentCollection(artId[1]))
 
-    article.appendChild(newCommentSection)
+    artId[0].appendChild(newCommentSection)
 }
